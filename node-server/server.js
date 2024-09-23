3f86e52e143922276a6d85add5012aad46796efc
Rithesh-S <150768558+Rithesh-S@users.noreply.github.com>
2024-09-23 12:21:33 +0530

    Update server.js

--- node-server/server.js
+++ node-server/server.js
@@ -6,7 +6,7 @@ const chatifyRoutes = require('./routes/chatifyRoutes')
 const socket = require('./controllers/socket')
 
 const corsOptions = {
-    origin: 'https://chatify-5zrh0s5h5-rithesh-ss-projects.vercel.app/',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE' ],
 }
 
