2618289f6a0a01bd3556c00e54283e9a1c6ae17c
Rithesh-S <150768558+Rithesh-S@users.noreply.github.com>
2024-09-25 16:02:34 +0530

    Update timeStamp.js

--- node-server/utils/timeStamp.js
+++ node-server/utils/timeStamp.js
@@ -1,14 +1,17 @@
 function timeStampGenerator() {
     const now = new Date();
-    const timestamp = now.getFullYear() + '-' +
-                      ('0' + (now.getMonth() + 1)).slice(-2) + '-' +
-                      ('0' + now.getDate()).slice(-2) + ' ' +
-                      ('0' + now.getHours()).slice(-2) + ':' +
-                      ('0' + now.getMinutes()).slice(-2) + ':' +
-                      ('0' + now.getSeconds()).slice(-2)
-    return timestamp
+    const utcTime = now.getTime() + (now.getTimezoneOffset() * 60000);
+    const istTime = new Date(utcTime + (5.5 * 60 * 60 * 1000));
+    const timestamp = istTime.getFullYear() + '-' +
+                      ('0' + (istTime.getMonth() + 1)).slice(-2) + '-' +
+                      ('0' + istTime.getDate()).slice(-2) + ' ' +
+                      ('0' + istTime.getHours()).slice(-2) + ':' +
+                      ('0' + istTime.getMinutes()).slice(-2) + ':' +
+                      ('0' + istTime.getSeconds()).slice(-2);
+    
+    return timestamp;
 }
 
 module.exports = {
     timeStampGenerator
-}
+};
